72f3e2091edc510a3cc16b788342adb946e699a4
MSc-ComputerScience <msccomputersciencesystem@gmail.com>
2025-05-30 13:50:11 +0500

    Add files via upload

---
+++ JavaHome/Sample.java
@@ -0,0 +1,15 @@
+
+public class Sample
+{
+  public static void main(String arg[])
+  {
+   String fname="Ajith";
+   String lname="Kumar";
+   String fullname=fname.concat(lname);
+   System.out.println(fname.concat(lname));
+   System.out.println(fullname);
+   
+  
+    
+  }
+}
